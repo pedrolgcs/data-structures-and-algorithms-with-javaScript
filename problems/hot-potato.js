706468b9f21740f29f93eb8c7a81f1ddf9277353
Pedro Henrique <pedro.lg.cs@gmail.com>
2022-02-06 12:23:19 -0300

    refactor: use for each

--- problems/hot-potato.js
+++ problems/hot-potato.js
@@ -1,30 +1,30 @@
 import { Queue } from '../data-structures/queue/queue.js';
 
-function hotPotato(elementList = [], num) {
+function hotPotato(elementList = [], num = Math.floor(Math.random() * 10) + 1) {
   const queue = new Queue();
   const eliminatedList = [];
 
-  for (let index = 0; index < elementList.length; index++) {
-    queue.enqueue(elementList[index]);
-  }
+  elementList.forEach((element) => queue.enqueue(element));
 
   while (queue.size() > 1) {
+    // pass the potato
     for (let index = 0; index < num; index++) {
       const element = queue.dequeue();
       queue.enqueue(element);
     }
 
+    // remove player
     eliminatedList.push(queue.dequeue());
   }
 
   return {
     eliminated: eliminatedList,
-    winner: queue.dequeue(),
+    winner: queue.peek(),
   };
 }
 
 const names = ['John', 'Jack', 'Camila', 'Ingrid', 'Carl'];
 
-const result = hotPotato(names, 5);
+const result = hotPotato(names);
 
 console.log(`The winner is: ${result.winner}`);
