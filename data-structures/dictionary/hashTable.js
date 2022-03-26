295e931eaa38811c4d15eb07876677fa3dddea76
Pedro Henrique <pedro.lg.cs@gmail.com>
2022-03-26 11:39:11 -0300

    feat: implement toString method

--- data-structures/dictionary/hashTable.js
+++ data-structures/dictionary/hashTable.js
@@ -50,6 +50,40 @@ class HashTable {
     }
     return false;
   }
+
+  size() {
+    return Object.keys(this.table).length;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    const keys = Object.keys(this.table);
+
+    const objString = keys.map((key) => `${key} => ${this.table[key]}`).join(', \n');
+    return objString;
+  }
 }
+
+const hash = new HashTable();
+
+hash.put('Ygritte', 'ygritte@email.com');
+hash.put('Jonathan', 'jonathan@email.com');
+hash.put('Jamie', 'jamie@email.com');
+hash.put('Jack', 'jack@email.com');
+hash.put('Jasmine', 'jasmine@email.com');
+hash.put('Jake', 'jake@email.com');
+hash.put('Nathan', 'nathan@email.com');
+hash.put('Athelstan', 'athelstan@email.com');
+hash.put('Sue', 'sue@email.com');
+hash.put('Aethelwulf', 'aethelwulf@email.com');
+hash.put('Sargeras', 'sargeras@email.com');
+
+console.log(hash.toString());
 
 export { HashTable };
