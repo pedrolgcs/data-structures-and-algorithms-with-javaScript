f4cf89d9ce68970a77896c2c8525b1ed06bc57ff
Pedro Henrique <pedro.lg.cs@gmail.com>
2022-03-29 14:37:04 -0300

    feat: add remove method

---
+++ data-structures/dictionary/hashTableSeparateChaining.js
@@ -0,0 +1,86 @@
+import { LinkedList } from '../linkedList/linked-list.js';
+import { defaultToString } from '../../shared/util.js';
+import { ValuePair } from '../../shared/models/value-pair.js';
+
+class HashTableSeparateChaining {
+  toStrFn;
+  table;
+
+  constructor(toStrFn = defaultToString) {
+    this.toStrFn = toStrFn;
+    this.table = {};
+  }
+
+  #loseloseHashCode(key) {
+    if (typeof key === 'number') {
+      return key;
+    }
+    const tableKey = this.toStrFn(key);
+    let hash = 0;
+
+    for (let i = 0; i < tableKey.length; i++) {
+      hash += tableKey.charCodeAt(i);
+    }
+
+    return hash % 37;
+  }
+
+  #hashCode(key) {
+    return this.#loseloseHashCode(key);
+  }
+
+  put(key, value) {
+    if (key != null && value != null) {
+      const position = this.#hashCode(key);
+      if (this.table[position] == null) {
+        this.table[position] = new LinkedList();
+      }
+      this.table[position].push(new ValuePair(key, value));
+      return true;
+    }
+    return false;
+  }
+
+  get(key) {
+    const position = this.#hashCode(key);
+    const linkedList = this.table[position];
+    if (linkedList != null && !linkedList.isEmpty()) {
+      let current = linkedList.getHead();
+      do {
+        if (current.element.key === key) {
+          return current.element.value;
+        }
+        current = current.next;
+      } while (current != null);
+    }
+    return undefined;
+  }
+
+  remove(key) {
+    const position = this.#hashCode(key);
+    const linkedList = this.table[position];
+    if (linkedList != null && !linkedList.isEmpty()) {
+      let current = linkedList.getHead();
+      do {
+        if (current.element.key === key) {
+          linkedList.remove(current.element);
+          if (linkedList.isEmpty()) {
+            delete this.table[position];
+          }
+          return true;
+        }
+        current = current.next;
+      } while (current !== null);
+    }
+    return false;
+  }
+}
+
+const hash = new HashTableSeparateChaining();
+
+hash.put('Jonathan', 'jonathan@email.com');
+hash.put('Jamie', 'jamie@email.com');
+hash.put('Sue', 'sue@email.com');
+hash.remove('Sue');
+
+export { HashTableSeparateChaining };
