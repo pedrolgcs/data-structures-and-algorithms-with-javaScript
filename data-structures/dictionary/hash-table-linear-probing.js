1db17d47da56d09346e630c08d5cf4db30ee144c
Pedro Henrique <pedro.lg.cs@gmail.com>
2022-05-11 11:46:14 -0300

    put examples

--- data-structures/dictionary/hash-table-linear-probing.js
+++ data-structures/dictionary/hash-table-linear-probing.js
@@ -88,15 +88,23 @@ class HashTableLinearProbing extends HashTableSeparateChaining {
 
 const hash = new HashTableLinearProbing();
 
+hash.put('Ygritte', 'ygritte@email.com');
 hash.put('Jonathan', 'jonathan@email.com');
 hash.put('Jamie', 'jamie@email.com');
+hash.put('Jack', 'jack@email.com');
+hash.put('Jasmine', 'jasmine@email.com');
+hash.put('Jake', 'jake@email.com');
+hash.put('Nathan', 'nathan@email.com');
+hash.put('Athelstan', 'athelstan@email.com');
 hash.put('Sue', 'sue@email.com');
-hash.put('Gandalf', 'white@email.com');
-hash.put('Dragon', 'red@email.com');
+hash.put('Aethelwulf', 'aethelwulf@email.com');
+hash.put('Sargeras', 'sargeras@email.com');
 
 console.log(hash.table);
 
-hash.remove('Jamie');
+hash.remove('Jonathan');
+
+console.log('------------------------------------------------------');
 
 console.log(hash.table);
 
